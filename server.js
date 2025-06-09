dbbe01e89db3ced134451889b2fcf5b4009b7f11
sharma <ajay.sharma171@outlook.com>
2025-06-09 23:09:28 +0530

    corrected a small typo

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ connectDB();
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+  console.log(
+    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
+  );
 });
 
 // Handle unhandled promise rejections
